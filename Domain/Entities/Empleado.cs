f53ca933c881dc6051d52cf20dfe0b2a3e12c16f
NinjaSaidR1820 <saidrivas1820@hotmail.com>
2022-03-31 09:40:11 -0600

    Class Empleado & Enums IEmpleadoModel & BinartyEmpleadoModel

--- Domain/Entities/Empleado.cs
+++ Domain/Entities/Empleado.cs
@@ -10,9 +10,12 @@ namespace Domain.Entities
     public class Empleado
     {
 
-        public int Codigo { get ; set; }
-        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
         public string Telefono { get; set; }
+        public string Email { get; set; }
         public Estado Estado { get; set; }
     }
 }
